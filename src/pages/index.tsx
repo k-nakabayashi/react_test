1c391b14642e884b1271bb66b045465e9aef7c83
nakabayashi kotaro <123windam@gmail.com>
2022-05-22 14:19:09 +0900

    env

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import '../style/index.scss';
+
+import React from 'react';
+
+const IndexPage = function () {
+  return (
+    <React.Fragment>
+      <header></header>
+      <main className="test">
+        <section>
+          <h1>h1 aaaaa</h1>
+        </section>
+        <article>
+          <section>
+            <h2>h2 title</h2>
+            <p>
+              aadada
+              <br />
+              aadada
+              <br />
+              aadada
+              <br />
+              aadada
+              <br />
+              aadada
+            </p>
+          </section>
+        </article>
+      </main>
+      <footer>footer aa</footer>
+    </React.Fragment>
+  );
+};
+
+export default IndexPage;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module: {
+rules: [
+    {
+        test: /\.scss$/,
+        use: [
+            {
+                loader: 'style-loader'
+            }, 
+            {
+                loader: 'css-loader',
+                modules: true,
+            }, 
+            {
+                loader: 'sass-loader',
+                implementation: require('sass'),
+                sassOptions: {
+                    fiber: require('fibers'),
+                },
+            }
+        ]
+    }
+  ]
+}
